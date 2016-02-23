22ceeca867ca4fa3573bf31c3fde289b1aee43b6
abdulhannanijaz <abdulhannanijaz@gmail.com>
2016-02-24 01:02:19 +0500

    home

--- Turtle/TurtleDAL/Validation.cs
+++ Turtle/TurtleDAL/Validation.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TurtleDAL
 {
@@ -11,7 +7,21 @@ namespace TurtleDAL
     {
 
         [Required]
+        [Display(Name="Clan Name",Description ="Name of clan")]
+        [StringLength(50,ErrorMessage ="between 0 and 50 characters",MinimumLength =3)]
+        [DataType(DataType.Text)]
+
         public string Name { get; set; }
+      
+        [Display(Name = "Symbol Picture", Description = "Logo of clan")]
+        [DataType(DataType.Upload)]
+     
+        public string SymbolPic { get; set; }
+
+    
+        [Display(Name = "Is Clan Evil ?", Description = "nature of clan")]
+
+        public Nullable<bool> IsEvil { get; set; }
 
     }
 }
